7e3291b9c0f5c5a61e2b40824a08796ed965dbd0
Paul McCherry <paul_mccherry@yahoo.com>
2015-09-22 09:24:22 +0100

    Before Changing walk directions to be correct

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	
+	private Rigidbody2D rBody ;
+	Animator anim ;
+	// Use this for initialization
+	void Start () {
+		rBody = GetComponent<Rigidbody2D> () ;
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Vector2 movementVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) ;
+		
+		if (movementVector != Vector2.zero) {
+			anim.SetBool("IsWalking",true) ;
+			anim.SetFloat ("InputX", movementVector.x) ;
+			anim.SetFloat ("InputY", movementVector.y) ;
+		} else {
+			anim.SetBool("IsWalking",false) ;
+		}
+		
+		rBody.MovePosition(rBody.position + movementVector * Time.deltaTime) ;	 
+	
+	}
+}
